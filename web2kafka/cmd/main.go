83ad101a4a1b4884d2e0c87b4004b586d644918c
vadim16795 <explosivedust@yandex.ru>
2025-09-22 22:46:59 +0300

    producer

--- web2kafka/cmd/main.go
+++ web2kafka/cmd/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"time"
-
-	"github.com/gin-gonic/gin"
+	"web2kafka/producer"
 )
 
 func main() {
-	r := gin.Default()
-
-	gin.DisableConsoleColor()
-
-	// API routes
-	v1 := r.Group("/api/v1")
-	{
-		v1.GET("/health", HealthCheck)
-	}
-
-	r.Run(":8080")
-}
-
-func HealthCheck(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status":    "ok",
-		"timestamp": time.Now().UTC().Format(time.RFC3339),
-	})
+	producer.KafkaSend()
 }
---
+++ web2kafka/producer/producer.go
@@ -0,0 +1,40 @@
+package producer
+
+import (
+	"context"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func KafkaSend() {
+
+	// make a writer that produces to topic-A, using the least-bytes distribution
+	w := &kafka.Writer{
+		Addr:     kafka.TCP("192.168.1.151:9093"),
+		Topic:    "topic-A",
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	err := w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte("Hello World!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-B"),
+			Value: []byte("One!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-C"),
+			Value: []byte("Two!"),
+		},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
